0d154b30443e637563a3b06a4ebbdb5f9b518195
liciniopsj <licinio.psj@gmail.com>
2022-08-24 18:19:48 -0300

    requisito 8

--- src/challenges.js
+++ src/challenges.js
@@ -1,31 +1,31 @@
-// Desafio 1
+// Desafio 1*
 function compareTrue(p1,p2) {
   return p1 && p2;
 }
 
-// Desafio 2
+// Desafio 2*
 function calcArea(base,height) {
   return (base * height) / 2;
 }
 
-// Desafio 3
+// Desafio 3*
 function splitSentence(sentence) {
   let result = sentence.split(" ");
   return result;
 }
 
-// Desafio 4
+// Desafio 4*
 function concatName(arrayOfStrings) {
   return arrayOfStrings[arrayOfStrings.length -1].concat(", ", arrayOfStrings[0]);
 }
 
-// Desafio 5
+// Desafio 5*
 function footballPoints(wins,ties) {
   let result = (wins * 3) + ties;
   return result;
 }
 
-// Desafio 6
+// Desafio 6*
 function checkHighestNumber(arrayOfNumbers){ //Encontra o maior valor no array
   let compare = arrayOfNumbers[0]; // iniciado com o primeiro valor do array pra não ter problema com numeros negativos.
   
@@ -56,11 +56,12 @@ function highestCount(arrayOfNumbers) { // Conta e devolve os hits.
   return hits;
 }
 
-// Desafio 7
+// Desafio 7*
 function catAndMouse(mouse,cat1,cat2) {
   let cat1Pos = 0, cat2Pos = 0;
   cat1Pos = Math.abs(cat1 - mouse);
   cat2Pos = Math.abs(cat2 - mouse);
+  //Math.abs - Absolute number / https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 
   if (cat1Pos === cat2Pos) return 'os gatos trombam e o rato foge';
   if (cat1Pos < cat2Pos) return 'cat1';
@@ -70,9 +71,23 @@ function catAndMouse(mouse,cat1,cat2) {
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+// let test = [2, 15, 7, 9, 45];
+function fizzBuzz(aryOfNumbers) {
+  let result = [];
+  
+  for (let index = 0;index < aryOfNumbers.length; index += 1){
+    if (aryOfNumbers[index] % 3 === 0 && aryOfNumbers[index] % 5 === 0){
+      result.push('fizzBuzz');
+    }else{
+      if (aryOfNumbers[index] % 3 === 0) result.push('fizz');
+      if (aryOfNumbers[index] % 5 === 0) result.push('buzz');
+    }
+    if (!(aryOfNumbers[index] % 3 === 0) && !(aryOfNumbers[index] % 5 === 0)) result.push('bug!');
+  }
+  
+  return result;
 }
+// fizzBuzz(test);
 
 // Desafio 9
 function encode() {
