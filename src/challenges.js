1b0beee80ce6b08c6db694e73fd4c6903d6fc0c2
liciniopsj <licinio.psj@gmail.com>
2022-08-24 16:20:24 -0300

    requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -27,7 +27,7 @@ function footballPoints(wins,ties) {
 
 // Desafio 6
 function checkHighestNumber(arrayOfNumbers){ //Encontra o maior valor no array
-  let compare = arrayOfNumbers[0]; // iniciado com o primeiro valor do array pra não ter problema com negativos.
+  let compare = arrayOfNumbers[0]; // iniciado com o primeiro valor do array pra não ter problema com numeros negativos.
   
   for (let index in arrayOfNumbers){
      if (arrayOfNumbers[index] > compare){
@@ -57,8 +57,16 @@ function highestCount(arrayOfNumbers) { // Conta e devolve os hits.
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse,cat1,cat2) {
+  let cat1Pos = 0, cat2Pos = 0;
+  cat1Pos = Math.abs(cat1 - mouse);
+  cat2Pos = Math.abs(cat2 - mouse);
+
+  if (cat1Pos === cat2Pos) return 'os gatos trombam e o rato foge';
+  if (cat1Pos < cat2Pos) return 'cat1';
+
+  return 'cat2';
+
 }
 
 // Desafio 8
