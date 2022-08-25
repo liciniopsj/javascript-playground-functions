94054c79ebb69f0ecc27d8db789a8e83fb570d83
liciniopsj <licinio.psj@gmail.com>
2022-08-25 13:45:09 -0300

    Desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,8 +9,18 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(inString) {
+  let numberExtractor = /\d+/g; //Parametros pro .match
+  let numStringAry = inString.match(numberExtractor);
+  let numberAry = numStringAry.map(Number); // convert to number
+  let sumResult = 0;
+
+  for (let sum of numberAry){
+    sumResult += sum;
+  }
+  if (sumResult === 1) return 1 + ' copo de água';
+  return sumResult + ' copos de água'
+  
 }
 
 module.exports = {
